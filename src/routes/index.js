8baf598ebc42a0dd0d66427ad10667ce82a045d4
rveldman <rveldman@pointlogic.com>
2019-08-20 21:21:37 +0200

    move code to src folder

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var packageJson = require('../package.json');
+var packageJson = require('../../package.json');
 var router = express.Router();
 
 /* GET home page. */
