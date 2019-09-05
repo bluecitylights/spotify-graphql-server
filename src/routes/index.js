439527520cce46ff8a9601cf5cc66eb624cda530
rveldman <rveldman@pointlogic.com>
2019-09-05 17:14:23 +0200

    secure callback with randomn string. support redirect from client

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,6 +3,7 @@ var packageJson = require('../../package.json');
 var router = express.Router();
 let request = require('request')
 let querystring = require('querystring')
+const STATE_KEY = 'spotify_auth_state';
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -14,39 +15,50 @@ let redirect_uri =
   'http://localhost:4000/callback'
 
   router.get('/login', function(req, res) {
-  console.log('login');
+    const state = JSON.stringify({
+      random: Math.random().toString(16),
+      redirect: req.query.client_redirect_uri
+    })
+    res.cookie(STATE_KEY, state);
     res.redirect('https://accounts.spotify.com/authorize?' +
     querystring.stringify({
       response_type: 'code',
       client_id: process.env.SPOTIFY_CLIENT_ID,
       scope: 'user-read-private user-read-email',
       redirect_uri,
-      state: req.query.client_redirect_uri
+      state: state
     }));
 })
 
 router.get('/callback', function(req, res) {
-  console.log('callb')
-  let code = req.query.code || null
-  let authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    form: {
-      code: code,
-      redirect_uri,
-      grant_type: 'authorization_code'
-    },
-    headers: {
-      'Authorization': 'Basic ' + (new Buffer(
-        process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
-      ).toString('base64'))
-    },
-    json: true
+  const { code, state } = req.query;
+  const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+  // state state validation
+  if (state === null || state !== storedState) {
+    res.redirect('/#/error/state mismatch');
+  } else {
+    res.clearCookie(STATE_KEY);
+    let authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (Buffer.from(
+          process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+        ).toString('base64'))
+      },
+      json: true
+    }
+    request.post(authOptions, function(error, response, body) {
+      const redirect = JSON.parse(state).redirect;
+      //res.redirect(`${redirect}user/${body.access_token}/${body.refresh_token}`);
+      res.redirect(`${redirect}?access_token=${body.access_token}`)
+    });
   }
-  request.post(authOptions, function(error, response, body) {
-    var access_token = body.access_token
-    let uri = req.query.state/*process.env.FRONTEND_URI*/ || 'http://localhost:3000'
-    res.redirect(uri + '?access_token=' + access_token)
-  })
-})
+});
+  
 
 module.exports = router;
