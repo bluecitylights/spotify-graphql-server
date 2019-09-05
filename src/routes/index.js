8d9e39390bc22346097cb862d198d17b04cd3332
bluecitylights <30653230+bluecitylights@users.noreply.github.com>
2019-09-05 17:39:36 +0200

    Auth (#27)
    * improve auth
    
    * secure callback with randomn string. support redirect from client

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,21 +1,64 @@
 var express = require('express');
 var packageJson = require('../../package.json');
 var router = express.Router();
+let request = require('request')
+let querystring = require('querystring')
+const STATE_KEY = 'spotify_auth_state';
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Spotify Graphql Server', version: packageJson.version });
 });
 
+let redirect_uri = 
+  process.env.REDIRECT_URI || 
+  'http://localhost:4000/callback'
 
-const authorizeSpotify = require('../datasources/spotify/auth/authorize');
-const getAccessToken = require('../datasources/spotify/auth/getAccessToken');
+  router.get('/login', function(req, res) {
+    const state = JSON.stringify({
+      random: Math.random().toString(16),
+      redirect: req.query.client_redirect_uri
+    })
+    res.cookie(STATE_KEY, state);
+    res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: process.env.SPOTIFY_CLIENT_ID,
+      scope: 'user-read-private user-read-email',
+      redirect_uri,
+      state: state
+    }));
+})
 
-router.get('/login', authorizeSpotify);
-router.get('/callback', getAccessToken, (req, res, next) => {
-  //res.redirect(`${clientUrl}/?authorized=true`);
-  res.redirect(`/graphql?access_token=${req.credentials.access_token}`); // todo use session
-  console.log(`successfull authenticated`);
+router.get('/callback', function(req, res) {
+  const { code, state } = req.query;
+  const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
+  // state state validation
+  if (state === null || state !== storedState) {
+    res.redirect('/#/error/state mismatch');
+  } else {
+    res.clearCookie(STATE_KEY);
+    let authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'Authorization': 'Basic ' + (Buffer.from(
+          process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+        ).toString('base64'))
+      },
+      json: true
+    }
+    request.post(authOptions, function(error, response, body) {
+      const redirect = JSON.parse(state).redirect;
+      //res.redirect(`${redirect}user/${body.access_token}/${body.refresh_token}`);
+      res.redirect(`${redirect}?access_token=${body.access_token}`)
+    });
+  }
 });
+  
 
 module.exports = router;
